45e544e608267525223db094286f8a35cab70c21
Aleksandr Lazarchuk <aleksandr.lazarchuk@escalibre.net>
2019-01-20 23:49:46 +0200

    master

---
+++ README.md
@@ -0,0 +1,9 @@
+Как написать SQL-запрос на Slick и не открыть портал в ад
+======================
+
+* https://habr.com/company/cit/blog/305682
+* https://github.com/forketyfork/hell-slick-demo
+* ( http://slick.lightbend.com/doc/1.0.1/gettingstarted.html#populating-the-database )
+
+
+Исходный код к статье ["Как написать SQL-запрос на Slick и не открыть портал в ад"](https://habrahabr.ru/company/cit/blog/305682/).
---
+++ src/main/scala/com/forketyfork/hellslick/ListDemo.scala
@@ -0,0 +1,36 @@
+package com.forketyfork.hellslick
+
+object ListDemo extends App {
+
+  // списки
+  val people = List("Воронин", "Гейгер", "Убуката")
+  val positions = List("мусорщик", "следователь", "редактор", "xxx")
+
+
+  // декартово произведение списков с использованием for-включения:
+  val peoplePositions1 = for { //todo:  умный вложенный цыкл (он определяет размер каждого из списков)
+    person <- people      //todo:  список String-ов возвращает String
+    position <- positions //todo:  список String-ов возвращает String
+  } yield s"$person = $position" //todo:  объединяем (контенируем) строки...
+  println( peoplePositions1 )
+  println( "#0  " + peoplePositions1(0) ) //todo  доступ к элементу списка по индексу
+  println( "#1  " + peoplePositions1(1) )
+  println( "#2  " + peoplePositions1(2) )
+
+
+  val peoplePositions2 = people.map { //todo:  вложенный цыкл в отдельном 1-потоке
+    person =>
+      positions.map {
+        position => s"$person = $position" //todo:  объединяем (контенируем) строки...
+      }
+  }
+  println(peoplePositions2)
+  // декартово произведение списков прямым вызовом flatMap и map:
+  val peoplePositions3 = people.flatMap { //todo:  вложенный цыкл в отдельных N-потоков
+    person =>
+      positions.map {
+        position => s"$person = $position" //todo:  объединяем (контенируем) строки...
+    }
+  }
+  println(peoplePositions2)
+}
